845d32cc9882b482cce38b39f376a4041a4dd633
shovaaa <shova.723@gmail.com>
2020-07-27 09:01:01 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Cart from './Cart'
+
+const items = [
+  {id:1, name: 'Taco Seasoning', price: 2.25, qty: 2},
+  {id: 2, name: 'Pinto Beans', price: 1.99, qty: 3},
+  {id: 3, name: 'Sour Cream', price: 3.50, qty: 1},
+]
+function App() {
+  return (
+    <div>
+      <Cart initialItems={items}/>
+    </div>
+  );
+}
+
+export default App;
